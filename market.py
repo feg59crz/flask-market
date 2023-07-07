b3cc22775752f8a32617c0471e49cba7e261250a
feg59crz <fernandogomescruz@gmail.com>
2023-07-07 19:00:26 -0300

    market page

--- market.py
+++ market.py
@@ -10,4 +10,9 @@ def home_page():
 
 @app.route("/market")
 def market_page():
-    return render_template('market.html')
+    items = [
+        {"id": 1, "name": "Phone", "barcode": "8932546468", "price": 500},
+        {"id": 2, "name": "Laptop", "barcode": "8932530945", "price": 900},
+        {"id": 3, "name": "Keyboard", "barcode": "8932523467", "price": 150},
+    ]
+    return render_template('market.html', items=items)
