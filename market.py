2f46bbfb776d628b5405827b9598491cc1300638
feg59crz <fernandogomescruz@gmail.com>
2023-07-07 14:20:19 -0300

    dinamicaly routes example

--- market.py
+++ market.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# dinamicaly routes - to receive some string after about page
+@app.route("/about/<username>")
+def about_page(username):
+    return f'<h1>This is the about page of {username}</h1>'
